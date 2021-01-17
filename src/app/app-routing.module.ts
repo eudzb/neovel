218bfdc605eef3fe4e53762fb3c98efe683683c0
Eudrey Bandelaria <eudreybandelaria@gmail.com>
2021-01-17 20:53:30 +0100

    added book info component to routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { MainPageComponent } from './pages/main-page/main-page.component';
-
+import { MainPageComponent } from '../app/pages/main-page/main-page.component';
+import { BookInfoPageComponent } from '../app/pages/book-info-page/book-info-page.component';
 
 const routes: Routes = [
-  { path: "main-page", component: MainPageComponent },
-  { path: "", component: MainPageComponent },
-  { path: "**", component: MainPageComponent }
+  { path: 'main-page', component: MainPageComponent },
+  { path: 'book-info/:bookId', component: BookInfoPageComponent },
+  { path: '', redirectTo: '/main-page', pathMatch: 'full' },
+  { path: '**', component: MainPageComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
