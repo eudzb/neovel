a4b95c52b0bcee715e7f5d1422af1ff08616ec03
Eudrey Bandelaria <eudreybandelaria@gmail.com>
2021-01-17 20:53:52 +0100

    added book info

--- src/app/components/book-list/book-list.component.ts
+++ src/app/components/book-list/book-list.component.ts
@@ -1,18 +1,23 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
+import { Book } from '../../models/book';
 
 @Component({
   selector: 'app-book-list',
   templateUrl: './book-list.component.html',
-  styleUrls: ['./book-list.component.scss']
+  styleUrls: ['./book-list.component.scss'],
 })
 export class BookListComponent implements OnInit {
+  @Input() id: number;
   @Input() name: string;
-  @Input() genre: string[];
+  @Input() genres: string[];
   @Input() authors: string[];
 
-  constructor() { }
+  constructor(private router: Router) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
+  goToBook = () => {
+    this.router.navigate(['/book-info/', this.id ]);
+  };
 }
