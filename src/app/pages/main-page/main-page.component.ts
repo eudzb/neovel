9cd52e90a8522f2d8cbfdeac7557eddee9445054
Eudrey Bandelaria <eudreybandelaria@gmail.com>
2021-01-17 16:23:05 +0100

    main page done

--- src/app/pages/main-page/main-page.component.ts
+++ src/app/pages/main-page/main-page.component.ts
@@ -1,6 +1,7 @@
 import { ThrowStmt } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { BookService } from 'src/app/services/book.service';
+import { Book } from '../../models/book';
 
 @Component({
   selector: 'app-main-page',
@@ -8,46 +9,15 @@ import { BookService } from 'src/app/services/book.service';
   styleUrls: ['./main-page.component.scss'],
 })
 export class MainPageComponent implements OnInit {
-  books: any = [
-    {
-      id: 3695,
-      name: 'Midnight Moonlight',
-      genre: ["Horror, Harem, Romance"],
-      authors: ["Eren Reverie"],
-    },
-    {
-      id: 7005,
-      name: 'V is for Venice',
-      genre: [],
-      authors: ["Bailey Elizabeth"],
-    },
-    {
-      id: 6011,
-      name: 'Forge of Destiny',
-      genre: [],
-      authors: ["Yrsillar"],
-    },
-    {
-      id: 6801,
-      name: 'V is for Violet',
-      genre: [],
-      authors: ["Bailey Elizabeth"],
-    },
-  ];
+  books: Book[];
 
   constructor(private bookService: BookService) { }
 
   ngOnInit(): void {
-    // this.bookService.getMostReadBooks().subscribe(books => {
-    //   console.log(books);
-    //   // this.books = books;
-    // })
-    // this.bookService.getMostReadBooks().subscribe();
-    // this.bookService.getBookDetails(4).subscribe(res => console.log(res));
-    // this.bookService.getBookChapters(4).subscribe(res => console.log(res));
+    this.books = this.bookService.books;
   }
 
-  onPress = () => {
+  getMostReadBooks = () => {
     this.bookService.getMostReadBooks().subscribe(res => console.log(res));
   }
 }
--- src/app/services/book.service.ts
+++ src/app/services/book.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { map } from 'rxjs/operators';
+import { Book } from '../models/book';
 
 @Injectable({
   providedIn: 'root',
@@ -11,6 +12,58 @@ export class BookService {
   headerOption = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
   };
+
+  books: Book[] = [
+    {
+      id: 3898,
+      name: 'Star Child',
+      genre: ['Action', 'Adventure', 'Fantasy'],
+      authors: ['Leonard'],
+    },
+    {
+      id: 3695,
+      name: 'Midnight Moonlight',
+      genre: ['Horror', 'Harem', 'Romance', 'Supernatural'],
+      authors: ['Eren Reverie'],
+    },
+    {
+      id: 3905,
+      name: 'Dirge',
+      genre: ['Action', 'Sci-Fi', 'Fantasy', 'Tragedy'],
+      authors: ['Shaeor'],
+    },
+    {
+      id: 3873,
+      name: 'Domina',
+      genre: ['Action', 'Adventure', 'Supernatural'],
+      authors: ['Discar'],
+    },
+    {
+      id: 3903,
+      name: 'The Fifth Defiance',
+      genre: [],
+      authors: ['Walter'],
+    },
+    {
+      id: 3904,
+      name: 'Daybreak on Hyperion',
+      genre: [],
+      authors: ['Aorii'],
+    },
+    {
+      id: 6777,
+      name: 'Occidendum',
+      genre: [],
+      authors: ['Thibault Guemard3'],
+    },
+    {
+      id: 3955,
+      name: 'Chosen Shackles',
+      genre: ['Action', 'Sci-fi', 'Horror', 'Tragedy'],
+      authors: ['Shaeor'],
+    },
+  ];
+
   constructor(private http: HttpClient) {}
 
   getMostReadBooks = () => {
